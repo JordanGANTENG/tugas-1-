d9d19fd14e42c2149826f9f37994b08ed802d991
Jordan <mufrihjordan@gmail.com>
2024-11-01 09:15:15 +0800

    Add files via upload

---
+++ tugas4.py
@@ -0,0 +1,4 @@
+print("Nama saya Mufrihzahy Jordan Aslam")
+print("Nim saya 24241167")
+print("saya dari desa Jurang Sate")
+print("Jumlah 2 digit nim awal ditambah 2 digit terakhir yaitu 24 + 67 = 91")
