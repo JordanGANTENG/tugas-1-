edc1feafaefb6d2513d04d2c40b5c89e42afe8e0
Jordan <mufrihjordan@gmail.com>
2024-11-08 10:24:37 +0800

    Update and rename tugas6.py to Prog6.py

--- Prog6.py
+++ Prog6.py
@@ -10,8 +10,8 @@ JordanX = 10 # boleh gabungan capital non-capital
 jordanX = 5
 jordan99 = 10 # jika menggabungkan huruf dan angka, angkanya di akhir
 print("nilai dari jordan adalah : ", jordan99)
-   
 
+#tipe data
 a = 4 # integer bilangan bulat
 print("tipe data dari a : ", type(a))
 
@@ -19,9 +19,9 @@ b = 6.9 # float bilangan pecahan
 print("tioe data dari b :",type(b))
 
 c = "gacor" # string,gabungan karakter
-print("tipe dsts dari c :", type(c))
+print("tipe data dari c :", type(c))
 
 d = false # boolean, true false
-print("tipe dsri d :", type(d))
+print("tipe data dari d :", type(d))
 
 
