d410f8067b39fe6ac82237e8acc29e6a7bfa424b
Jordan <mufrihjordan@gmail.com>
2024-11-08 10:02:31 +0800

    Add files via upload

---
+++ tugas6.py
@@ -0,0 +1,27 @@
+
+x = 9
+y = 6
+print ("nilai dari x adalah : ", y)
+
+#aturan penamaan variabel
+luas = 60 # bisa berupa kata
+jordan_keren = 99 # jika dua kata, digabung dengan "_"
+JordanX = 10 # boleh gabungan capital non-capital
+jordanX = 5
+jordan99 = 10 # jika menggabungkan huruf dan angka, angkanya di akhir
+print("nilai dari jordan adalah : ", jordan99)
+   
+
+a = 4 # integer bilangan bulat
+print("tipe data dari a : ", type(a))
+
+b = 6.9 # float bilangan pecahan
+print("tioe data dari b :",type(b))
+
+c = "gacor" # string,gabungan karakter
+print("tipe dsts dari c :", type(c))
+
+d = false # boolean, true false
+print("tipe dsri d :", type(d))
+
+
