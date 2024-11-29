3c6fc91c100d1ecd0283b1e2f3e59c6980e1f7e5
Jordan <mufrihjordan@gmail.com>
2024-11-29 10:04:00 +0800

    Add files via upload

---
+++ Tugas8.py
@@ -0,0 +1,33 @@
+# Aritmatika
+
+x = 24
+y = 67
+
+
+# operasi pengurangan 
+hasil = x - y 
+print ("nilai dari x - y = ", hasil)
+
+# operasi penjumlahan
+hasil = x + y
+print ("nilai dari x + y = ", hasil)
+
+# operasi pembagian
+hasil = x / y
+print ("nilai dari x / y = ", hasil)
+
+# operasi perkalian
+hasil = x * y
+print ("nilai dari x * y = ", hasil)
+
+# eksponen (perpangkatan)
+hasil = x ** y
+print ("nilai dari x ** y = ", hasil)
+
+# modulus (sisa bagi)
+hasil = x % y
+print ("nilai dari x % y = ", hasil)
+
+# floor division
+hasil = x // y
+print ("nilai dari x // y = ", hasil)
