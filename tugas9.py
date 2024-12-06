b9f21abc383cd9fed2754ea334041391f149ecd4
Jordan <mufrihjordan@gmail.com>
2024-12-06 09:46:47 +0800

    Add files via upload

---
+++ tugas9.py
@@ -0,0 +1,14 @@
+# latihan operasi aritmatika
+
+nilai =float(input("masukkan nilai suhu dalam celcius :"))
+
+fahrenheit = (nilai * 9/5)+32
+print("nilai dari fahrenheut= ",fahrenheit)
+
+
+kelvin = (nilai + 273,15)
+print("nilai dari kelvin = ",kelvin)
+
+
+reamur = (nilai (4/5) * celcius
+print(f"nilai dari reamur: {reamur}r)
